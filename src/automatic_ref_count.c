a24d5afc94b7ea401000c3585a39177f78c9b4ac
VladSima <>
2021-01-14 09:39:34 +0200

    Moved the allocate/retain/release into a separate file.

---
+++ src/automatic_ref_count.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <pthread.h>
+#include "automatic_ref_count.h"
+
+// instead of using a simple lock, we can use a dictionary to store the lock per each object (or the control block to sync retain/release calls) [void*: pthread_mutex_t]
+// init the dictionary when allocate is first called, then each time there is a new object create a new entry and pass it to it :D
+static pthread_mutex_t lock;
+
+void *allocate(size_t bytes_count) 
+{
+    void *allocated_block = malloc(bytes_count + 1);
+    uint8_t *reference_count = (uint8_t *)allocated_block;
+    *reference_count = 1;
+
+    return (void*)((char *)allocated_block + 1);
+}
+
+void retain(void *address) 
+{
+    uint8_t *reference_count = (uint8_t *)((char *)(address) - 1);
+    pthread_mutex_lock(&lock);
+    reference_count ++;
+    pthread_mutex_unlock(&lock);
+}
+
+void release(void *address)
+{
+    uint8_t *reference_count = (uint8_t *)((char *)(address) - 1);
+    pthread_mutex_lock(&lock);
+    reference_count --;
+    pthread_mutex_unlock(&lock);
+
+    if (reference_count == 0)
+    {
+        free(((char *)(address) - 1));
+    }
+}
---
+++ src/automatic_ref_count.h
@@ -0,0 +1,11 @@
+
+#ifndef AUTOMATIC_REF_COUNT
+#define AUTOMATIC_REF_COUNT
+
+// define here types if needed
+
+#endif
+
+void *allocate(size_t bytes_count);
+void retain(void *address);
+void release(void *address);
